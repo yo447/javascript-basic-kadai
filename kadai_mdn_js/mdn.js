b45f36105e17a9006748375ce79b4e488140e5c9
yo447 <kakuto1019@gmail.com>
2025-04-27 11:21:48 +0900

    kadai_mdn_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const birthday = new Date();
+
+const date1 = birthday.getDate();
+const year = birthday.getFullYear();
+const month = birthday.getMonth() + 1;
+
+console.log(year + "年" + month + "月" + date1 + "日");
+
+
+
